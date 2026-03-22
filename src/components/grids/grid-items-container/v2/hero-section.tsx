5a51efed04c46e0c1d9a0c9ca1fabc0713e21c9b
Timviecxanh <Timviecxanh@gmail.com>
2026-03-22 15:56:26 +0700

    edit

--- src/components/grids/grid-items-container/v2/hero-section.tsx
+++ src/components/grids/grid-items-container/v2/hero-section.tsx
@@ -1,7 +1,6 @@
 import classes from "./hero-section.module.scss";
 import Image from "next/image";
 
-// TẠO GIẢ ĐỊNH HEADER: Bạn hãy thay bằng Component Header thật của bạn nhé!
 import Header from "@/components/primitives/search-bar/search-bar";
 
 export default function HeroSection() {
@@ -15,9 +14,9 @@ export default function HeroSection() {
 
       <div className={classes.bannerContainer}>
         <Image
-          src="/images/group1.png" // Nhớ thay đúng tên file ảnh của bạn
+          src="/images/group1.png"
           alt="Jena Hair Events Banner"
-          width={1259} // Khai báo đúng kích thước bạn muốn
+          width={1259}
           height={786}
           priority
           sizes="100vw"
